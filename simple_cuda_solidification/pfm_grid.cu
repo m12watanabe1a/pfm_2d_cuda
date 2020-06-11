a848c0a7491afc7d67f050208259fd4aeac61fc4
m12watanabe1a <m12watanabe1a@gmail.com>
2020-06-11 18:16:27 +0900

    mv all params to const memory field

--- simple_cuda_solidification/pfm_grid.cu
+++ simple_cuda_solidification/pfm_grid.cu
@@ -8,41 +8,40 @@
 #include <fstream>
 
 const unsigned int field_size = 53;
-const unsigned int x_length = field_size;
-const unsigned int y_length = field_size;
-const float dx = 5e-7;
-const float dy = 5e-7;
-const float beta = .5;
 const unsigned int step = 100;
 
+__device__ unsigned int d_field_size;
+__device__ float d_dx;
 __device__ float d_a;
 __device__ float d_w;
+__device__ float d_beta;
 __device__ float d_tau;
 
 __global__ void calc_step(double *d_phase, double *d_phase_tmp) {
   int x_i = blockIdx.x * blockDim.x + threadIdx.x;
   int y_i = blockIdx.y * blockDim.y + threadIdx.y;
-  if (x_i <= 0 || x_i >= x_length - 1 || y_i <= 0 || y_i >= y_length - 1) return;
+  if (x_i <= 0 || x_i >= d_field_size - 1 || y_i <= 0 || y_i >= d_field_size - 1) return;
 
-  int i = y_i * x_length + x_i;
-  double rpx = (d_phase[i + 1] - 2.* d_phase[i] + d_phase[i - 1]) / (dx * dx);
-  double rpy = (d_phase[i + x_length] - 2. * d_phase[i] + d_phase[i - x_length]) / (dy * dy);
+  int i = y_i * d_field_size + x_i;
+  float ddx = d_dx * d_dx;
+  double rpx = (d_phase[i + 1] - 2.* d_phase[i] + d_phase[i - 1]) / ddx;
+  double rpy = (d_phase[i + d_field_size] - 2. * d_phase[i] + d_phase[i - d_field_size]) / ddx;
 
   double dpi1 = d_a * d_a * (rpx + rpy);
-  double dpi2 = 4. * d_w * d_phase[i] * (1 - d_phase[i]) * (d_phase[i] - .5 + beta);
+  double dpi2 = 4. * d_w * d_phase[i] * (1 - d_phase[i]) * (d_phase[i] - .5 + d_beta);
   double dpi = dpi1 + dpi2;
   d_phase_tmp[i] = d_phase[i] + d_tau * dpi;
 }
 
 // 境界条件
 double* set_bc(double *phase) {
-  for (unsigned int x_i = 0; x_i < x_length; x_i++) {
-    phase[x_i] = phase[x_length + x_i];
-    phase[(y_length -1) * x_length + x_i] = phase[(y_length - 2) * x_length + x_i];
+  for (unsigned int x_i = 0; x_i < field_size; x_i++) {
+    phase[x_i] = phase[field_size + x_i];
+    phase[(field_size -1) * field_size + x_i] = phase[(field_size - 2) * field_size + x_i];
   }
-  for (unsigned int y_i = 0; y_i < y_length; y_i++) {
-    phase[y_i * x_length] = phase[y_i * x_length + 1];
-    phase[(y_i + 1)*x_length - 1] = phase[(y_i + 1)*x_length - 2];
+  for (unsigned int y_i = 0; y_i < field_size; y_i++) {
+    phase[y_i * field_size] = phase[y_i * field_size + 1];
+    phase[(y_i + 1)*field_size - 1] = phase[(y_i + 1)*field_size - 2];
   }
   return phase;
 }
@@ -56,9 +55,9 @@ bool save(double *phase, unsigned int n) {
 
     file << "#x #y #phase" << std::endl;
     // remove boundaries
-    for (unsigned int y_i = 1; y_i < y_length - 1; y_i++) {
-      for (unsigned int x_i = 1; x_i < x_length - 1; x_i++) {
-        file << y_i << ' ' <<  x_i << ' ' << phase[y_i * x_length + x_i] << std::endl;
+    for (unsigned int y_i = 1; y_i < field_size - 1; y_i++) {
+      for (unsigned int x_i = 1; x_i < field_size - 1; x_i++) {
+        file << y_i << ' ' <<  x_i << ' ' << phase[y_i * field_size + x_i] << std::endl;
       }
       file << std::endl;
     }
@@ -71,12 +70,13 @@ bool save(double *phase, unsigned int n) {
 }
 
 int main() {
-  unsigned int N = y_length * x_length;
+  unsigned int N = field_size * field_size;
   double *phase; // phase field for host
   double *d_phase, *d_phase_tmp; // phase field for device
 
   phase = (double *)malloc(N * sizeof(double));
 
+  const float dx = 5e-7;
   // 界面エネルギー
   float gamma = 1.;
   // 界面幅
@@ -96,23 +96,28 @@ int main() {
   float dt = dx * dx / (5. * M_phi * a * a);
   printf("Time Step: %.3e[s]\n", dt);
   // 固相初期半径
-  float r0 = .5 * (x_length - 1) * dx;
+  float r0 = .5 * (field_size - 1) * dx;
+
+  float beta = .5;
 
   float tau = M_phi * dt;
 
+  cudaMemcpyToSymbol(d_field_size, &field_size, sizeof(unsigned int));
+  cudaMemcpyToSymbol(d_dx, &dx, sizeof(float));
   cudaMemcpyToSymbol(d_a, &a, sizeof(float));
   cudaMemcpyToSymbol(d_w, &w, sizeof(float));
+  cudaMemcpyToSymbol(d_beta, &beta, sizeof(float));
   cudaMemcpyToSymbol(d_tau, &tau, sizeof(float));
 
   // 初期条件セット
-  for (unsigned int y_i = 0; y_i < y_length; y_i++) {
-    for (unsigned int x_i = 0; x_i < x_length; x_i++) {
-      int i = y_i * x_length + x_i;
-      if (x_i <= 0 || x_i >= x_length - 1 || y_i <= 0 || y_i >= y_length - 1) {
+  for (unsigned int y_i = 0; y_i < field_size; y_i++) {
+    for (unsigned int x_i = 0; x_i < field_size; x_i++) {
+      int i = y_i * field_size + x_i;
+      if (x_i <= 0 || x_i >= field_size - 1 || y_i <= 0 || y_i >= field_size - 1) {
         phase[i] = 0.0;
         continue;
       }
-      float y = (y_i - 1) * dy;
+      float y = (y_i - 1) * dx;
       float x = (x_i - 1) * dx;
 
       float r = std::sqrt(x*x + y*y) - r0;
